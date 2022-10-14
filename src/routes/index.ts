acf0cbe79748ae8d8c5d2c3ea1edf4438af9f162
Rafael Omodei <rafael.omodei@dasa.com.br>
2022-10-14 14:10:05 -0300

    removed specification

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import { loadsRoutes } from './loads.routes';
-import { specificationRoutes } from './specification.routes';
 
 const router = Router();
 
 router.use('/loads', loadsRoutes);
-router.use('/specification', specificationRoutes);
 
 export { router };
